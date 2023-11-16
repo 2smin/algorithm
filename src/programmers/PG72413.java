76aa358edb25f06c437b894fd1d689c204c4fd1a
2smin <planner263@gmail.com>
2023-11-16 20:53:00 +0900

    add solved

--- src/programmers/PG72413.java
+++ src/programmers/PG72413.java
@@ -12,14 +12,15 @@ public class PG72413 {
                 answer);
     }
     static int[][] map;
-    static Node[] nodeList;
+    static Node[] aNodeList;
+    static Node[] bNodeList;
+    static Node[] sNodeList;
 
     class Node implements Comparable{
-        int num = 0;
+        int position = 0;
         int fare = 100001;
         boolean visited = false;
         boolean inQueue = false;
-        List<Integer> checkpoint = new ArrayList<>();
 
         @Override
         public int compareTo(Object o) {
@@ -31,7 +32,6 @@ public class PG72413 {
     }
 
     public int solution(int n, int s, int a, int b, int[][] fares) {
-
         /*
         필요한것
         index별로 다녀온 노드를 넣을 수 있는 이중 queue
@@ -40,6 +40,9 @@ public class PG72413 {
          */
 
         map = new int[n+1][n+1];
+        aNodeList  = new Node[n+1];
+        bNodeList = new Node[n+1];
+        sNodeList = new Node[n+1];
 
         for(int[] fare : fares){
             int from = fare[0];
@@ -48,51 +51,71 @@ public class PG72413 {
             map[from][to] = map[to][from] = money;
         }
 //
-                Arrays.stream(map).forEach(strs -> { Arrays.stream(strs).forEach(str -> System.out.print(str + "   "));
-            System.out.println();});
+//                Arrays.stream(map).forEach(strs -> { Arrays.stream(strs).forEach(str -> System.out.print(str + "   "));
+//            System.out.println();});
 
-        nodeList = new Node[n+1];
+        dji(sNodeList,s,n);
+        dji(aNodeList,a,n);
+        dji(bNodeList,b,n);
+
+//        print(sNodeList);
+//        print(aNodeList);
+//        print(bNodeList);
+
+        int answer = 20000001;
+        for(int i=0; i<n+1; i++){
+            int currentTotal = sNodeList[i].fare + aNodeList[i].fare + bNodeList[i].fare;
+            answer = Math.min(answer,currentTotal);
+        }
+        return answer;
+    }
+
+    public void dji(Node[] nodeList, int from, int n){
+
         for(int i=0; i<n+1; i++){
             Node node = new Node();
-            node.num = i;
-            node.fare = 10001;
+            node.position = i;
+            node.fare = 1000001;
+            node.visited = false;
+            node.inQueue = false;
             nodeList[i] = node;
         }
+        /*
+        시작 노드를 세팅하고 넣는다.
+        해당 노드에서 map을 보고 접근 가능한 노드 (0이 아닌 노드) 를 거리를 찾는다
+        해당 노드의 거리를 업데이트한다 직전 노드의 거리까지 더해서 해당 노드의 거리보다 작은 경우 업데이트
+        그리고 priority queue에 넣는다. 이떄 isExist가 false인 경우만 넣는다.
+         */
 
-        Queue<Node> queue = new PriorityQueue(new Comparator() {
+        //start node 세팅
+        nodeList[from].fare = 0;
+        nodeList[from].visited = true;
+        nodeList[from].inQueue = true;
+
+        Queue<Node> queue = new PriorityQueue<>(new Comparator() {
             @Override
             public int compare(Object o1, Object o2) {
                 Node node1 = (Node)o1;
                 Node node2 = (Node)o2;
                 return node1.fare - node2.fare;
-            }
-        });
+            }});
+        queue.offer(nodeList[from]);
 
-        nodeList[s].num = s;
-        nodeList[s].fare = 0;
-        nodeList[s].visited = true;
-        nodeList[s].inQueue = true;
-
-        queue.offer(nodeList[s]);
         while(!queue.isEmpty()){
-            Node node = queue.poll();
-            node.visited = true;
-
-            System.out.println("node :" + node.num);
-            System.out.println("fare :" + node.fare);
-
-            int[] reachable = map[node.num];
-            for(int i=1; i<reachable.length; i++){
-                if(reachable[i] == 0) continue;
+            Node currentNode = queue.poll();
+            currentNode.visited = true;
+            //map에서 currentNode와 만나는 node 다 검색
+            int[] reachableNodes = map[currentNode.position];
+            for(int i=0; i<reachableNodes.length; i++){
+                //만나지않는 노드 패스
+                if(reachableNodes[i]==0) continue;
+                //이미 방문한 노드 패스
                 if(nodeList[i].visited) continue;
+                //이제 방문 가능한 노드만 남았다 여기서 거리 체크, 기존 요금보다 더 싼지, 더 싼요금이면 갈아끼우자
+                if(nodeList[i].fare > currentNode.fare + reachableNodes[i]){
+                    nodeList[i].fare = currentNode.fare + reachableNodes[i];
 
-                if(nodeList[i].fare > reachable[i] + node.fare){
-                    System.out.println("    nextNode : " + i);
-                    System.out.println("    reachable : " + reachable[i]);
-                    nodeList[i].fare = node.fare + reachable[i];
-                    System.out.println("    nextFare : " + nodeList[i].fare);
-                    nodeList[i].checkpoint = new ArrayList<>(node.checkpoint);
-                    nodeList[i].checkpoint.add(node.num);
+                    //이미 queue에 있으면 넣자
                     if(!nodeList[i].inQueue){
                         nodeList[i].inQueue = true;
                         queue.offer(nodeList[i]);
@@ -101,36 +124,14 @@ public class PG72413 {
             }
         }
 
-        Arrays.stream(map).forEach(strs -> { Arrays.stream(strs).forEach(str -> System.out.print(str + "   "));
-            System.out.println();});
+    }
 
-        for(Node node : nodeList){
-            node.checkpoint.add(node.num);
-            System.out.println("Num :" + node.num + " , fare : " + node.fare);
-            node.checkpoint.stream().forEach(System.out::print);
-            System.out.println();
-        }
 
-        int aFare = nodeList[a].fare;
-        int bFare = nodeList[b].fare;
-        int aSize = nodeList[a].checkpoint.size();
-        int bSize = nodeList[b].checkpoint.size();
+    public void print(Node[] list){
+        Arrays.stream(list).forEach(node -> {
+            System.out.print(node.fare +", ");
+        });
 
-        int sameNode = 100001;
-        for(int i=0; i<Math.min(aSize,bSize); i++){
-            if(nodeList[a].checkpoint.get(i) != nodeList[b].checkpoint.get(i)){
-                break;
-            }else{
-                sameNode = nodeList[a].checkpoint.get(i);
-            }
-        }
-
-        int answer = 0;
-        if(sameNode!=10001){
-            answer = aFare + bFare - nodeList[sameNode].fare;
-        }else{
-            answer = aFare + bFare;
-        }
-        return answer;
+        System.out.println();
     }
 }
